cfa1af2b572f4f2d0aa000c0eb189f7faf299762
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-22 10:35:29 -0600

    implemented 16 by 16 square grids

---
+++ etch-a-sketch.js
@@ -0,0 +1,9 @@
+// get id container
+const container = document.getElementById('container');
+
+// create grid
+for(let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+}
