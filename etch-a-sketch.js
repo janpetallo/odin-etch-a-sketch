1addc6e19135c2c90d4cdb865b4c4572df1f4174
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-22 10:42:21 -0600

    added hover effect on the grids

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,5 +5,9 @@ const container = document.getElementById('container');
 for(let i = 0; i < 16 * 16; i++) {
     const div = document.createElement('div');
     div.classList.add('square');
+    // change color on hover
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    });
     container.appendChild(div);
 }
